4a537bc22ab9180232c0cb87c77d8434cff03cbc
kaduh15 <kadu.silva2014@gmail.com>
2023-07-10 13:06:32 -0300

    ✨ feat: add prisma orm

---
+++ app/backend/src/prisma/seed.ts
@@ -0,0 +1,25 @@
+import prisma from "./prisma-client"
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@admin.com' },
+    update: {},
+    create: {
+      email: 'admin@admin.com',
+      name: 'Admin',
+      password: 'admin',
+      role: 'ADMIN',
+      
+    },
+  })
+  console.log({ admin })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
