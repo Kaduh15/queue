80487d3a5b46978546d4063a05e2e88f72d1e6a3
kaduh15 <kadu.silva2014@gmail.com>
2023-07-13 16:37:41 -0300

    🔒 chore(auth.middleware.ts): refactor authMiddleware to use TypeScript types for role and payload 🐛 fix(auth.middleware.ts): fix incorrect usage of headers.get() method, change it to headers.get() to retrieve Authorization header value

--- app/backend/src/middlewares/auth.middleware.ts
+++ app/backend/src/middlewares/auth.middleware.ts
@@ -3,12 +3,18 @@ import { NextFunction } from 'express'
 import { Auth } from '@/lib/jsonwebtoken'
 import { NotFoundError, UnauthorizedError } from '@/utils/http-errors'
 
-export default function authMiddleware(role?: string) {
+type Role = 'ADMIN' | 'USER'
+
+type AuthPayload = {
+  role: Role
+}
+
+export default function authMiddleware(role?: Role) {
   return async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.get('Authorization')?.split(' ').at(-1)
     if (!token) throw new NotFoundError('token not found')
 
-    const payload = Auth.verify(token)
+    const payload = Auth.verify<AuthPayload>(token)
 
     if (role && payload.role !== role) {
       throw new UnauthorizedError('unauthorized')
