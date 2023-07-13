dc1105f13793fcf1c594a76756f289ab24e000a6
kaduh15 <kadu.silva2014@gmail.com>
2023-07-13 16:37:09 -0300

    🔒 chore(jsonwebtoken.ts): improve type safety and default value handling in Auth class 🔒 chore(jsonwebtoken.ts): improve type safety and default value handling in Auth class

--- app/backend/src/lib/jsonwebtoken.ts
+++ app/backend/src/lib/jsonwebtoken.ts
@@ -1,18 +1,23 @@
+import { Payload } from '@prisma/client/runtime/library'
 import dotenv from 'dotenv'
-import jwt, { Secret } from 'jsonwebtoken'
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken'
 
 dotenv.config()
 
+type AuthPayload<PayloadType = object> = PayloadType & JwtPayload
+
 export class Auth {
   private static readonly secret = process.env.JWT_SECRET as Secret
-  private static readonly expiresIn = process.env.JWT_EXPIRES_IN as string
+  private static readonly expiresIn = process.env.JWT_EXPIRES_IN
   private static jwt = jwt
 
-  static sign(payload: object | string, expiresIn?: string | number) {
-    return this.jwt.sign(payload, this.secret, { expiresIn: this.expiresIn })
+  static sign(payload: object | string, expiresIn: string | number = '1d') {
+    return this.jwt.sign(payload, this.secret, {
+      expiresIn: this.expiresIn || expiresIn,
+    })
   }
 
-  static verify(token: string) {
-    return this.jwt.verify(token, this.secret)
+  static verify<PayloadType = object>(token: string): AuthPayload<PayloadType> {
+    return this.jwt.verify(token, this.secret) as AuthPayload<PayloadType>
   }
 }
