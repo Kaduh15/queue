32d10b15fe9cb6e49379991db6b7b588ab7617ed
kaduh15 <kadu.silva2014@gmail.com>
2023-07-25 19:51:55 -0300

    ✨ feat(base.entity.ts): add BaseEntity class and IBaseEntity interface to serve as a base for other entities
    ✨ feat(base.repository.ts): create BaseRepository interface with CRUD methods for working with BaseEntity and its subclasses

---
+++ app/backend/src/repositories/base.repository.ts
@@ -0,0 +1,12 @@
+import { BaseEntity } from '@/entities/base.entity'
+
+export type Id = number
+
+export interface BaseRepository<T = object & BaseEntity> {
+  create(data: Omit<T, 'id'>): Promise<T>
+  getAll(): Promise<T[]>
+  getById(id: Id): Promise<T | undefined>
+  update(id: Id, data: T): Promise<T>
+  delete(id: Id): Promise<void>
+  deleteAll(): Promise<void>
+}
