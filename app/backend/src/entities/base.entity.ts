1cc0ae852de638dee4f70a38a1dfcacee25ad524
kaduh15 <kadu.silva2014@gmail.com>
2023-07-27 17:23:11 -0300

    🔀 refactor(base.entity.ts): convert IBaseEntity class to an interface for better code organization and readability

--- app/backend/src/entities/base.entity.ts
+++ app/backend/src/entities/base.entity.ts
@@ -1,15 +1,9 @@
 import { Id } from '@/repositories/base.repository'
 
-class IBaseEntity {
+interface IBaseEntity {
   id: Id
   createdAt?: Date
   updatedAt?: Date
-
-  constructor(data: BaseEntity) {
-    this.id = data.id
-    this.createdAt = data.createdAt
-    this.updatedAt = data.updatedAt
-  }
 }
 
 export class BaseEntity implements IBaseEntity {
