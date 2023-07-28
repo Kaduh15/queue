b7d0d581ebcca50b109cffdaccfdd880df4dbcc7
kaduh15 <kadu.silva2014@gmail.com>
2023-07-27 21:37:42 -0300

    🔄 refactor(queue.entity.ts): rename 'phone' property to 'phoneNumber' for better clarity and consistency

--- app/backend/src/entities/queue.entity.ts
+++ app/backend/src/entities/queue.entity.ts
@@ -2,15 +2,15 @@ import { BaseEntity } from './base.entity'
 
 export default class Queue extends BaseEntity {
   name: string
-  phone: string
+  phoneNumber: string
 
-  constructor({ id, name, createdAt, updatedAt, phone }: Queue) {
+  constructor({ id, name, createdAt, updatedAt, phoneNumber }: Queue) {
     super({
       id,
       createdAt,
       updatedAt,
     })
     this.name = name
-    this.phone = phone
+    this.phoneNumber = phoneNumber
   }
 }
