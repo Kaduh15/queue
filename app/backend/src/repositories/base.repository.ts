0b337cc465682124553830477722ced792306549
kaduh15 <kadu.silva2014@gmail.com>
2023-07-28 14:35:36 -0300

    🐛 fix(base.repository.ts): exclude 'createdAt' and 'updatedAt' fields from the 'create' method to prevent overwriting these values

--- app/backend/src/repositories/base.repository.ts
+++ app/backend/src/repositories/base.repository.ts
@@ -3,7 +3,7 @@ import { BaseEntity } from '@/entities/base.entity'
 export type Id = number
 
 export interface BaseRepository<T = object & BaseEntity> {
-  create(data: Omit<T, 'id'>): Promise<T>
+  create(data: Omit<T, 'id' | 'createdAt' | 'updatedAt'>): Promise<T>
   getAll(): Promise<T[]>
   getById(id: Id): Promise<T | undefined>
   update(id: Id, data: Partial<T>): Promise<T>
