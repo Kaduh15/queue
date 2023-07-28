e9458b51a4d92babf3e2ce1112709a55ba8f7ac5
kaduh15 <kadu.silva2014@gmail.com>
2023-07-28 16:15:08 -0300

    🔒 chore(queue-router): add authentication middleware to POST / route to restrict access to ADMIN users 🔧 chore(queue-router): refactor POST / route to include authentication middleware before validation and controller functions

--- app/backend/src/routes/queue-router/index.ts
+++ app/backend/src/routes/queue-router/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import authMiddleware from '@/middlewares/auth.middleware'
 import bodyValidation from '@/middlewares/body-validation.middleware'
 import { QueuePrismaRepository } from '@/repositories/queue-repository/queue-prisma.repository'
 
@@ -15,7 +16,12 @@ const queueController = new QueueController(queueService)
 
 const queueRouter = Router()
 
-queueRouter.post('/', bodyValidation(createQueueSchema), queueController.create)
+queueRouter.post(
+  '/',
+  authMiddleware('ADMIN'),
+  bodyValidation(createQueueSchema),
+  queueController.create,
+)
 queueRouter.get('/today', queueController.getToday)
 
 export { queueRouter }
