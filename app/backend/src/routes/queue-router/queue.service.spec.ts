cbc1d54382d1c87710337708fd5f07d8fb37b2c0
kaduh15 <kadu.silva2014@gmail.com>
2023-07-28 15:14:18 -0300

    🚀 feat(queue.service.spec.ts): add beforeEach block to reset mocks, use real timers, and delete all queues before each test case 🔬 test(queue.service.spec.ts): add async/await to 'Should create a queue' test case to properly handle promises 🔬 test(queue.service.spec.ts): add 'getToday' test case to test the 'getToday' method of QueueService 🔬 test(queue.service.spec.ts): add assertions to 'getToday' test case to ensure correct behavior

--- app/backend/src/routes/queue-router/queue.service.spec.ts
+++ app/backend/src/routes/queue-router/queue.service.spec.ts
@@ -10,6 +10,13 @@ describe('QueueService', () => {
 
   const queueService = new QueueService(queueRepository)
 
+  beforeEach(() => {
+    vi.resetAllMocks()
+    vi.useRealTimers()
+
+    queueRepository.deleteAll()
+  })
+
   it('Should be defined', () => {
     expect(queueService).toBeDefined()
   })
@@ -27,7 +34,7 @@ describe('QueueService', () => {
       vi.useRealTimers()
     })
 
-    it('Should create a queue', () => {
+    it('Should create a queue', async () => {
       const queueInput: CreateQueueSchema = {
         name: 'Any Name',
         phoneNumber: '99999999999',
@@ -42,9 +49,30 @@ describe('QueueService', () => {
         updatedAt: new Date(),
       }
 
-      const queue = queueService.create(queueInput)
+      const queue = await queueService.create(queueInput)
 
-      expect(queue).resolves.toEqual(queueOutput)
+      expect(queue).toEqual(queueOutput)
+    })
+  })
+
+  describe('getToday', () => {
+    it('should get all current day customers', async () => {
+      const queueInput: CreateQueueSchema = {
+        name: 'Any Name',
+        phoneNumber: '99999999999',
+      }
+
+      vi.setSystemTime(new Date('2021-01-01T00:00:00.000Z'))
+      await queueService.create(queueInput)
+
+      vi.setSystemTime(new Date('2021-01-02T00:00:00.000Z'))
+      await queueService.create(queueInput)
+      await queueService.create(queueInput)
+
+      const queues = await queueService.getToday()
+
+      expect(queues).toHaveLength(2)
+      expect(queues[0]).toHaveProperty('id', 3)
     })
   })
 })
