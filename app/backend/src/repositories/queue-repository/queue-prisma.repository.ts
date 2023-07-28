984bc1f4fd47a8c26bbc46408b8f5b4174b066b5
kaduh15 <kadu.silva2014@gmail.com>
2023-07-27 21:44:06 -0300

    🐛 fix(queue-prisma.repository.ts): throw ConflictError if queue with the same name already exists to prevent duplicate queues

--- app/backend/src/repositories/queue-repository/queue-prisma.repository.ts
+++ app/backend/src/repositories/queue-repository/queue-prisma.repository.ts
@@ -1,10 +1,21 @@
 import Queue from '@/entities/queue.entity'
 import prisma from '@/prisma/prisma-client'
+import { ConflictError } from '@/utils/http-errors'
 
 import { QueueRepository } from './queue.repository'
 
 export class QueuePrismaRepository implements QueueRepository {
   async create(data: Omit<Queue, 'id'>): Promise<Queue> {
+    const hasQueue = await prisma.queue.findFirst({
+      where: {
+        name: data.name,
+      },
+    })
+
+    if (hasQueue) {
+      throw new ConflictError('Queue already exists')
+    }
+
     const queue = await prisma.queue.create({
       data,
     })
