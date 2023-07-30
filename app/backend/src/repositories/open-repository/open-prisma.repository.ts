143741cd708a2945c7519bcd20c1d73058c6b7ce
kaduh15 <kadu.silva2014@gmail.com>
2023-07-30 10:44:20 -0300

    🐛 fix(open-prisma.repository.ts): implement create, getById, and deleteAll methods in OpenPrismaRepository 🔥 chore(open.respository.ts): remove unused OpenRepository file

--- app/backend/src/repositories/open-repository/open-prisma.repository.ts
+++ app/backend/src/repositories/open-repository/open-prisma.repository.ts
@@ -4,16 +4,28 @@ import prisma from '@/prisma/prisma-client'
 import { OpenRepository } from './open.repository'
 
 export class OpenPrismaRepository implements OpenRepository {
-  create(_data: Omit<Open, 'id' | 'createdAt' | 'updatedAt'>): Promise<Open> {
-    throw new Error('Method not implemented.')
+  async create(
+    data: Omit<Open, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Open> {
+    const open = await prisma.open.create({
+      data,
+    })
+
+    return new Open(open)
   }
 
   getAll(): Promise<Open[]> {
     throw new Error('Method not implemented.')
   }
 
-  getById(_id: number): Promise<Open | undefined> {
-    throw new Error('Method not implemented.')
+  async getById(id: number): Promise<Open | undefined> {
+    const open = await prisma.open.findUnique({
+      where: { id },
+    })
+
+    if (!open) return undefined
+
+    return new Open(open)
   }
 
   async update(id: number, data: Partial<Open>): Promise<Open> {
@@ -29,7 +41,7 @@ export class OpenPrismaRepository implements OpenRepository {
     throw new Error('Method not implemented.')
   }
 
-  deleteAll(): Promise<void> {
-    throw new Error('Method not implemented.')
+  async deleteAll(): Promise<void> {
+    await prisma.open.deleteMany()
   }
 }
--- app/backend/src/repositories/open-repository/open.respository.ts
+++
@@ -1,5 +0,0 @@
-import Open from '@/entities/open.entity'
-
-import { BaseRepository } from '../base.repository'
-
-export type OpenRepository = BaseRepository<Open>
