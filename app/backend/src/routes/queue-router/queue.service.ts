7be6d443c412bd525e73b16214541723df984309
kaduh15 <kadu.silva2014@gmail.com>
2023-07-31 15:34:51 -0300

    🔧 refactor(queue.service.ts): add OpenRepository dependency to QueueService constructor to improve code modularity and testability 🐛 fix(queue.service.ts): add validation to check if the queue is open before creating a new customer to prevent creating customers when the queue is closed

--- app/backend/src/routes/queue-router/queue.service.ts
+++ app/backend/src/routes/queue-router/queue.service.ts
@@ -1,12 +1,16 @@
+import { OpenRepository } from '@/repositories/open-repository/open.repository'
 import { QueueRepository } from '@/repositories/queue-repository/queue.repository'
+import { BadRequestError } from '@/utils/http-errors'
 
 import { CreateQueueSchema } from './schemas/queue-create.schema'
 
 export class QueueService {
   model: QueueRepository
+  openRepository: OpenRepository
 
-  constructor(model: QueueRepository) {
+  constructor(model: QueueRepository, openRepository: OpenRepository) {
     this.model = model
+    this.openRepository = openRepository
   }
 
   async getToday() {
@@ -16,6 +20,10 @@ export class QueueService {
   }
 
   async create(data: CreateQueueSchema) {
+    const isOpen = await this.openRepository.getById(1)
+
+    if (!isOpen?.isOpen) throw new BadRequestError('It is not open')
+
     const customer = await this.model.create(data)
 
     return customer
