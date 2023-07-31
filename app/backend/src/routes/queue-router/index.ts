5621e91f5f498d06a0d8eeb0222e4fca006246a5
kaduh15 <kadu.silva2014@gmail.com>
2023-07-31 15:38:13 -0300

    🔀 merge(queue-router): import 'openRepository' function from 'open-router' module to use in QueueService constructor 🔀 merge(queue-router): pass 'openRepository' function as a parameter to QueueService constructor to enable interaction with open queues

--- app/backend/src/routes/queue-router/index.ts
+++ app/backend/src/routes/queue-router/index.ts
@@ -4,13 +4,15 @@ import authMiddleware from '@/middlewares/auth.middleware'
 import bodyValidation from '@/middlewares/body-validation.middleware'
 import { QueuePrismaRepository } from '@/repositories/queue-repository/queue-prisma.repository'
 
+import { openRepository } from '../open-router'
+
 import { QueueController } from './queue.controller'
 import { QueueService } from './queue.service'
 import { createQueueSchema } from './schemas/queue-create.schema'
 
 export const queueRepository = new QueuePrismaRepository()
 
-const queueService = new QueueService(queueRepository)
+const queueService = new QueueService(queueRepository, openRepository)
 
 const queueController = new QueueController(queueService)
 
