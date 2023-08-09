c302d4af990c4e87a83e853a9f882dc500c8488c
kaduh15 <kadu.silva2014@gmail.com>
2023-08-09 08:37:02 -0300

    📦 chore(env.d.ts): add type definitions for Vite environment variables to improve type safety 🔧 chore(vite.config.ts): load environment variables from .env file and set the server port based on VITE_PORT environment variable or use default port if not provided

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,13 +1,22 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-})
+    server: {
+      port: Number(process.env.VITE_PORT) || undefined,
+    },
+  })
+}
