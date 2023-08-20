28ce6324a2572e85c2fa6c08c1c531c251c44df8
Kadu <kadu.silva2014@gmai.com>
2023-08-20 14:24:49 -0300

    ✨ feat(backend): create class WAClient

--- app/whatsapp-api/src/browser.ts
+++ app/whatsapp-api/src/browser.ts
@@ -1,110 +1,176 @@
-import puppeteer from 'puppeteer'
+import puppeteer, { Browser, Page } from 'puppeteer'
 import qrcodeterminal from 'qrcode-terminal'
 
 import 'dotenv/config'
 
-type SendMessageProps = {
-  message: string
+type createBrowserProps = {
+  headless?: boolean
+  executablePath?: string
+}
+
+type sendMessageProps = {
   phone: string
+  message: string
 }
 
-export async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
+export default class WAClient {
+  private browser: Browser | null = null
+  private pages: Map<string, Page> = new Map()
+  public isLogged = false
 
-export async function createBrowser() {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-    headless: process.env.NODE_ENV === 'production' ? 'new' : false,
-    executablePath: '/usr/bin/google-chrome-stable',
-    userDataDir: './tmp',
-  })
+  private async sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 
-  console.log(await browser.version())
-  console.log(await browser.userAgent())
-
-  return browser
-}
-
-export async function getQrCode() {
-  try {
-    const p = await page
-    await p.reload({
+  async init(
+    createData: createBrowserProps = {
+      headless: true,
+      executablePath: '/usr/bin/google-chrome-stable',
+    },
+  ) {
+    const browser = await this.createBrowser(createData)
+    this.browser = browser
+    const homePage = await this.browser.newPage()
+    await homePage.goto('https://web.whatsapp.com', {
       waitUntil: 'domcontentloaded',
     })
-    const classQrCode = '._19vUU[data-ref]'
-    const qrCodeElement = await p.waitForSelector(classQrCode, {
-      visible: true,
+    await this.sleep(1000)
+
+    this.pages.set('home', homePage)
+  }
+
+  async createBrowser(
+    {
+      headless = true,
+      executablePath = '/usr/bin/google-chrome-stable',
+    }: createBrowserProps = {
+      headless: true,
+      executablePath: '/usr/bin/google-chrome-stable',
+    },
+  ) {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      headless: headless ? 'new' : false,
+      executablePath,
     })
 
+    return browser
+  }
+
+  async getQrCode(qrCodeInTerminal = false) {
+    const logged = await this.hasLogged()
+    if (logged) {
+      return null
+    }
+
+    const page = this.pages.get('home')
+    if (!page) {
+      return null
+    }
+
+    await page.reload()
+
+    const qrCodeElement = await page.waitForSelector('._19vUU[data-ref]')
     const qrCode = await qrCodeElement?.evaluate((el) =>
       el.getAttribute('data-ref'),
     )
 
-    qrcodeterminal.generate(qrCode as string, { small: true })
+    if (!qrCode) {
+      return null
+    }
+
+    if (qrCode && qrCodeInTerminal) {
+      qrcodeterminal.generate(qrCode, { small: true })
+    }
 
     return qrCode
-  } catch (err) {
-    return null
   }
-}
 
-export async function isLogger() {
-  try {
-    const p = await page
-    await p.goto('https://web.whatsapp.com', {
-      waitUntil: 'domcontentloaded',
-    })
+  async login(qrCodeInTerminal = false) {
+    const page = this.pages.get('home')
+    if (!page) {
+      return
+    }
 
-    const loggerElement = await p.waitForSelector(
-      'span[data-icon="default-user"]',
-      {
-        timeout: 10000,
-      },
+    await page.reload()
+
+    await this.getQrCode(qrCodeInTerminal)
+
+    await page.waitForSelector('._19vUU', { hidden: true, timeout: 0 })
+    await this.sleep(5000)
+
+    const logged = await this.hasLogged()
+    console.log('🚀 ~ file: browser.ts:94 ~ WAClient ~ login ~ logged:', logged)
+  }
+
+  async hasLogged() {
+    if (this.isLogged) {
+      return this.isLogged
+    }
+
+    console.log('checking if has logged')
+    const page = this.pages.get('home')
+    if (!page) {
+      return false
+    }
+
+    const localStorage = await page.evaluate(() =>
+      Object.assign({}, window.localStorage),
     )
-    await sleep(1000)
 
-    return !!loggerElement
-  } catch (err) {
-    return false
+    const isLogged = !!localStorage['last-wid-md']
+    this.isLogged = isLogged
+    return isLogged
+  }
+
+  async sendMessage({ phone, message }: sendMessageProps) {
+    try {
+      if (!this.browser) {
+        throw new Error('Browser not found')
+      }
+      const sendPage = this.pages.get('home')
+      if (!sendPage) {
+        throw new Error('Page not found')
+      }
+
+      await sendPage.goto(
+        encodeURI(
+          `https://web.whatsapp.com/send?phone=${phone}&text=${message}`,
+        ),
+      )
+
+      const sendButton = await sendPage.waitForSelector(
+        'span[data-icon="send"]',
+        { timeout: 0 },
+      )
+      await this.sleep(1000)
+
+      await sendButton?.click()
+      await this.sleep(1000)
+    } catch (error) {
+      console.error(
+        '🚀 ~ file: browser.ts:138 ~ WAClient ~ sendMessage ~ error:',
+        error,
+      )
+    }
+  }
+
+  async closeBrowser() {
+    if (!this.browser) {
+      return
+    }
+
+    await this.browser.close()
+  }
+
+  async closePage(pageName: string) {
+    const page = this.pages.get(pageName)
+    if (!page) {
+      return
+    }
+
+    await page.close()
+
+    this.pages.delete(pageName)
   }
 }
-
-export async function sendMessage({ message, phone }: SendMessageProps) {
-  const p = await page
-  console.log('sendMessage', { message, phone })
-  await p.goto(`https://web.whatsapp.com/send?phone=${phone}`, {
-    waitUntil: 'domcontentloaded',
-  })
-
-  await p.waitForSelector('._3Uu1_')
-  await p.click('._3Uu1_', {
-    delay: 1000,
-  })
-  await p.keyboard.type(message, {
-    delay: 200,
-  })
-
-  await p.click('span[data-icon="send"]', {
-    delay: 1000,
-  })
-
-  await sleep(1000)
-}
-
-const browser = createBrowser()
-
-const page = browser.then(async (browser) => {
-  const p = await browser.newPage()
-
-  p.on('dialog', async (dialog) => {
-    console.log(dialog.message())
-    await dialog.dismiss()
-  })
-
-  p.on('popup', async (popup) => {
-    await popup.close()
-  })
-
-  return p
-})
--- app/whatsapp-api/src/server.ts
+++ app/whatsapp-api/src/server.ts
@@ -1,13 +1,21 @@
 import { app } from './app'
+import WAClient from './browser'
 
 import 'dotenv/config'
 
-import 'express-async-errors'
-
 const PORT = process.env.PORT
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 if (!PORT) {
   throw new Error('PORT is not defined!')
 }
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
+export const client = new WAClient()
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}!`)
+  await client.init({
+    headless: IS_PRODUCTION,
+  })
+  console.log('Client initialized!')
+})
