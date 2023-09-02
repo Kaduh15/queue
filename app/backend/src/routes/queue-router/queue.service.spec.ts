525721cc83b924ba62224d115caad4e2fe075372
Kadu <kadu.silva2014@gmai.com>
2023-09-02 16:12:57 -0300

    🧪 test(backend/queue.services.spec.ts): refactor to use sinon

--- app/backend/src/routes/queue-router/queue.service.spec.ts
+++ app/backend/src/routes/queue-router/queue.service.spec.ts
@@ -1,5 +1,7 @@
+import Sinon from 'sinon'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
+import Queue from '@/entities/queue.entity'
 import { OpenRepositoryInMemory } from '@/repositories/open-repository/open-in-memory.repository'
 import { QueueRepositoryInMemory } from '@/repositories/queue-repository/queue-in-memory.repository'
 
@@ -69,26 +71,29 @@ describe('QueueService', () => {
 
   describe('getToday', () => {
     it('should get all current day customers', async () => {
-      const queueInput: CreateQueueSchema = {
+      const queueInput: Omit<Queue, 'id'> = {
         name: 'Any Name',
         phoneNumber: '99999999999',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: 'WAITING',
       }
 
-      openRepository.update(1, {
-        isOpen: true,
-      })
-
-      vi.setSystemTime(new Date('2021-01-01T00:00:00.000Z'))
-      await queueService.create(queueInput)
-
-      vi.setSystemTime(new Date('2021-01-02T00:00:00.000Z'))
-      await queueService.create(queueInput)
-      await queueService.create(queueInput)
+      Sinon.stub(queueRepository, 'getToday').resolves([
+        {
+          id: 1,
+          ...queueInput,
+        },
+        {
+          id: 2,
+          ...queueInput,
+        },
+      ])
 
       const queues = await queueService.getToday()
 
       expect(queues).toHaveLength(2)
-      expect(queues[0]).toHaveProperty('id', 3)
+      expect(queues[0]).toHaveProperty('id', 1)
     })
   })
 })
