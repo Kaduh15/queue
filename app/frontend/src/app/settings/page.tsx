8df48187ed07de75bf55b8e103940679117736e8
Kaduh15 <kadu.silva2014@gmail.com>
2024-01-06 07:23:18 -0300

    feat(frontend): add cord for connect whatsapp

--- app/frontend/src/app/settings/page.tsx
+++ app/frontend/src/app/settings/page.tsx
@@ -1,4 +1,7 @@
+import Header from '@/components/Header'
 import QrCodeWhatsapp from '@/components/QrCodeWhatsapp'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default async function SettingsPage() {
   const data = await fetch(process.env.NEXT_PUBLIC_WHATSAPP_API + '/is-connected', {
@@ -7,9 +10,19 @@ export default async function SettingsPage() {
 
   return (
     <>
-      <h1>Settings</h1>
-      {data.connected && <h2>Connected</h2>}
-      {!data.connected && <QrCodeWhatsapp />}
+      <Header />
+      <main className='flex flex-col gap-5 my-5 px-5'>
+        <Card>
+          <CardHeader>
+            <CardTitle>Conectar ao WhatsApp</CardTitle>
+            {!data.connected && <CardDescription>Escaneie o QR Code abaixo para enviar mensagens de notificação para todos os clientes.</CardDescription>}
+          </CardHeader>
+          <CardContent className='flex items-center justify-center w-full'>
+            {data.connected && <h2>Conectado</h2>}
+            {!data.connected && <QrCodeWhatsapp />}
+          </CardContent>
+        </Card>
+      </main>
     </>
   )
 }
