4ecc4d5ed1cc774063cea924b5d117303c9f3f8d
Kaduh15 <kadu.silva2014@gmail.com>
2024-01-06 07:18:13 -0300

    feat(frontend): restricts access to settings route

---
+++ app/frontend/src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.has('token');
+  
+  if (token) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL('/', req.url));
+}
+
+export const config = {
+  matcher: ['/settings/:path*']
+}
