b4c1aae9e0692fb8ca11f50aca717422e275687d
Kadu <kadu.silva2014@gmail.com>
2024-07-06 14:25:42 -0300

    Feat: Adiciona mensagem com o salão está fechado

--- app/frontend/src/app/page.tsx
+++ app/frontend/src/app/page.tsx
@@ -7,11 +7,16 @@ import { cookies } from 'next/headers'
 export default async function Home() {
   const token = cookies().get('token')?.value
 
+  const { isOpen } = await fetch(process.env.API_URL + '/open', {
+    method: 'GET',
+  }).then((res) => res.json())
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-4">
       <Header />
       {token && <NextClientButton>Proximo</NextClientButton>}
-      <CardQueue />
+      {isOpen && <CardQueue />}
+      {!isOpen && <h1>Estabelecimento Fechado</h1>}
     </main>
   )
 }
